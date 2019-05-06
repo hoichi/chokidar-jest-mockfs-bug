5c93a028b139c2b28403b1b15942fee64f3f1abe
Sergey Samokhov <s.samokhov@aventica.ru>
2019-05-06 23:37:29 +0300

    Comments

--- chokidar_jest_mock-fs.test.js
+++ chokidar_jest_mock-fs.test.js
@@ -3,16 +3,24 @@ var fs = require('fs');
 var mockFs = require('mock-fs');
 
 describe('chokidar & mock-fs', () => {
+  /* setup */
   let path = 'path/to/file.txt';
   let content = 'The file content';
 
   beforeEach(() => mockFs({ [path]: content }));
   afterEach(mockFs.restore);
+  /* end setup */
 
+  /**
+   * This test runs ok
+   */
   it('fs.readFileSync', () => {
     expect(fs.readFileSync(path, { encoding: 'utf-8' })).toBe(content);
   });
 
+  /**
+   * THIS * ONE * THROWS *
+   */
   it('chokidar.watch', () => {
     return new Promise((resolve, reject) => chokidar.watch('path').on('add', () => resolve(expect('$20').toBe('$20'))));
   });
