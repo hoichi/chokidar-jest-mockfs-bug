d5ad597639f91f2605cadf3bd3d075d41552baec
Sergey Samokhov <s.samokhov@aventica.ru>
2019-05-06 23:19:25 +0300

    add readFileSync example

--- chokidar_jest_mock-fs.test.js
+++ chokidar_jest_mock-fs.test.js
@@ -1,10 +1,19 @@
-var mockFs = require('mock-fs');
 var chokidar = require('chokidar');
+var fs = require('fs');
+var mockFs = require('mock-fs');
 
-describe("chokidar & mock-fs", () => {
-  it("simple mock", () => {
-    mockFs({"path/to/file.txt": "the file content"});
-    chokidar.watch("path");
-  })
+describe('chokidar & mock-fs', () => {
+  let path = 'path/to/file.txt';
+  let content = 'The file content';
+
+  beforeEach(() => mockFs({ [path]: content }));
+  afterEach(mockFs.restore);
+
+  it('fs.readFileSync', () => {
+    expect(fs.readFileSync(path, { encoding: 'utf-8' })).toBe(content);
+  });
+
+  it('chokidar.watch', () => {
+    return new Promise((resolve, reject) => chokidar.watch('path').on('add', () => resolve(expect('$20').toBe('$20'))));
+  });
 });
-
